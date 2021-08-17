41a949f49422cb56665d34da36d1451e95c051b6
Arthur Scarpati <arthur_159@hotmail.com>
2021-08-17 18:31:50 -0300

    Added filter

--- src/app/courses/course-list.component.ts
+++ src/app/courses/course-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { CourseService } from "./course.service";
 import { Course } from "./courses";
 
 @Component({
@@ -7,31 +8,28 @@ import { Course } from "./courses";
 })
 
 export class CourseListComponent implements OnInit{
-       
-       courses: Course[] = [];
+
+       filteredCourses: Course[] = [];
+
+       _courses: Course[] = [];
+
+       _filterBy: string;
+
+       constructor(private courseService: CourseService){ }
 
        ngOnInit(): void {
-              this.courses = [
-                     {
-                            id: 1,
-                            name: 'Angular: Forms',
-                            imageUrl: '/assets/images/forms.png',
-                            price: 99.99,
-                            code: 'XPS-8796',
-                            duration: 120,
-                            rating: 4.5,
-                            releaseDate: 'December, 2, 2019'
-                     },
-                     {
-                            id: 2,
-                            name: 'Angular: HTTP',
-                            imageUrl: '/assets/images/http.png',
-                            price: 45.99,
-                            code: 'LKL-1094',
-                            duration: 80,
-                            rating: 4,
-                            releaseDate: 'December, 4, 2019'
-                     }
-              ]
+              this._courses = this.courseService.retrieveAll();
+              this.filteredCourses = this._courses;
        }
+
+       set filter(value: string){
+              this._filterBy = value;
+
+              this.filteredCourses = this._courses.filter((course: Course) => course.name.toLocaleLowerCase().indexOf(this._filterBy.toLocaleLowerCase()) > -1);
+       }
+
+       get filter(){
+              return this._filterBy;
+       }
+
 }
--- src/app/courses/courses.ts
+++ src/app/courses/courses.ts
@@ -6,5 +6,6 @@ export class Course {
        code: string;
        duration: number;
        rating: number;
+       description: string;
        releaseDate: string;
 }
